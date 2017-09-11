33889420d1749362e049bb1641c83eb4a6354933
Thiago Bianeck <bianeck27@gmail.com>
2017-09-11 15:59:36 -0300

    11092017
    15:59

--- node-str/src/controllers/product-controller.js
+++ node-str/src/controllers/product-controller.js
@@ -3,12 +3,22 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product.find({active: true}, 'title price slug')
+        .then(data => {
+            res.status(201).send(data);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+}
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
-    product
-        .save()
+    product.save()
         .then(x => {
-            res.status(201).send({message:'Produto cadastrado com sucesso'});
+            res.status(201).send({
+        message:'Produto cadastrado com sucesso'
+            });
         }).catch(e => {
             res.status(201).send({
                 message:'Falha ao cadastrar o Produto',
@@ -16,7 +26,7 @@ exports.post = (req, res, next) => {
             });
         });
 
-};
+}
 
 exports.put = (req, res, next)=>{
     const id = req.params.id;
